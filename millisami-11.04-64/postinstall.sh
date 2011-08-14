e6da004fadcad66ab9d93137c53572bc956332de
MillisamiSprout <sachin@sprout-technology.com>
2011-08-15 00:51:31 +0545

    updated postinstall.sh to use ruby 1.9.2 and rubygems 1.8.6

--- millisami-11.04-64/postinstall.sh
+++ millisami-11.04-64/postinstall.sh
@@ -22,22 +22,22 @@ apt-get -y install nfs-common
 # can install their own Rubies using packages or however.
 # We must install the 1.8.x series since Puppet doesn't support
 # Ruby 1.9 yet.
-wget http://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.7-p334.tar.gz
-tar xvzf ruby-1.8.7-p334.tar.gz
-cd ruby-1.8.7-p334
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz
+tar xvzf ruby-1.9.2-p290.tar.gz
+cd ruby-1.9.2-p290
 ./configure --prefix=/opt/ruby
 make
 make install
 cd ..
-rm -rf ruby-1.8.7-p334*
+rm -rf ruby-1.9.2-p290*
 
 # Install RubyGems 1.7.2
-wget http://production.cf.rubygems.org/rubygems/rubygems-1.7.2.tgz
-tar xzf rubygems-1.7.2.tgz
-cd rubygems-1.7.2
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.6.tgz
+tar xzf rubygems-1.8.6.tgz
+cd rubygems-1.8.6
 /opt/ruby/bin/ruby setup.rb
 cd ..
-rm -rf rubygems-1.7.2*
+rm -rf rubygems-1.8.6*
 
 # Installing chef & Puppet
 /opt/ruby/bin/gem install chef --no-ri --no-rdoc
